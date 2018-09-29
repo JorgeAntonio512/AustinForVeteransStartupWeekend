f0d47c6abf328a42d06cac5f7e3abc2caafd0e6b
georgepazdralwork <georgpaz@aol.com>
2018-09-29 11:35:41 -0500

    Initial Commit

---
+++ AustinForVeteransStartupWeekend/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AustinForVeteransStartupWeekend
+//
+//  Created by George Pazdral (work) on 9/29/18.
+//  Copyright © 2018 George Pazdral (work). All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AustinForVeteransStartupWeekend/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AustinForVeteransStartupWeekend
+//
+//  Created by George Pazdral (work) on 9/29/18.
+//  Copyright © 2018 George Pazdral (work). All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
