c589052df7f8c8cfdd751895ad7b116f38294821
georgepazdralwork <georgpaz@aol.com>
2018-11-02 21:58:43 -0500

    ..

--- AustinForVeteransStartupWeekend/SecondViewController.swift
+++ AustinForVeteransStartupWeekend/SecondViewController.swift
@@ -13,15 +13,20 @@ class SecondViewController: UIViewController {
 
     @IBOutlet var thanks: UITextView!
     @IBOutlet var instructions: UITextView!
+    @IBOutlet var hiThere: UITextView!
+    @IBOutlet var smileyFace: UITextView!
+    @IBOutlet var flag: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
    
+    
+        
         Database.database().reference().child("welcomePage").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             let value = snapshot.value as? NSDictionary
             let username = value?["thanks"] as? String ?? ""
-            self.thanks.text = username
+            //self.thanks.text = username
             
             // ...
         }) { (error) in
@@ -32,19 +37,24 @@ class SecondViewController: UIViewController {
             // Get user value
             let value = snapshot.value as? NSDictionary
             let usernamez = value?["instructions"] as? String ?? ""
-            self.instructions.text = usernamez
+            //self.instructions.text = usernamez
             
             // ...
         }) { (error) in
             print(error.localizedDescription)
         }
-        
-        
     }
+}
 
+extension UITextView {
     
+    func centerVertically() {
+        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fittingSize)
+        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+        let positiveTopOffset = max(1, topOffset)
+        contentOffset.y = -positiveTopOffset
+    }
     
-    
-
 }
 
